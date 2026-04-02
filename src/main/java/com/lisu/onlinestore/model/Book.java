9ae0dd7a5d0b7da181f4a658c3eb2993d04e90eb
alex <ursusandwolf@protonmail.com>
2026-04-02 14:28:10 +0300

    Book entity added

---
+++ src/main/java/com/lisu/onlinestore/model/Book.java
@@ -0,0 +1,16 @@
+package com.lisu.onlinestore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
