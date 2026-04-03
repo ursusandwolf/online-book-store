8175e690cd5859cd62557cf25826a45111392294
alex <ursusandwolf@protonmail.com>
2026-04-03 09:10:10 +0300

    BookRepositoryImpl save

--- src/main/java/com/lisu/onlinestore/dao/impl/BookRepositoryImpl.java
+++ src/main/java/com/lisu/onlinestore/dao/impl/BookRepositoryImpl.java
@@ -2,14 +2,38 @@ package com.lisu.onlinestore.dao.impl;
 
 import com.lisu.onlinestore.dao.BookRepository;
 import com.lisu.onlinestore.model.Book;
+import com.lisu.onlinestore.util.HibernateUtil;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     @Override
     public Book save(Book book) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can' t save book to DB");
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return book;
     }
 
     @Override
