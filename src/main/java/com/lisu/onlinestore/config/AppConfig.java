abcfa49c45113df166cfa676eb44b837006ae9f5
alex <ursusandwolf@protonmail.com>
2026-04-03 14:47:25 +0300

    properties added

--- src/main/java/com/lisu/onlinestore/config/AppConfig.java
+++ src/main/java/com/lisu/onlinestore/config/AppConfig.java
@@ -3,21 +3,28 @@ package com.lisu.onlinestore.config;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.lisu.onlinestore")
 public class AppConfig {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Root123$");
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
 
@@ -27,7 +34,7 @@ public class AppConfig {
         factoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("show_sql", "true");
+        properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.hbm2ddl.auto", "create-drop");
 
         factoryBean.setHibernateProperties(properties);
