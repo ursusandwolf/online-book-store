9775c9339ba0ada555e3608a99252385bb481e6b
alex <ursusandwolf@protonmail.com>
2026-04-13 13:00:53 +0300

    Validation is omitted

--- src/main/java/com/lisu/onlinestore/dto/BookRequestDto.java
+++ src/main/java/com/lisu/onlinestore/dto/BookRequestDto.java
@@ -1,9 +1,5 @@
 package com.lisu.onlinestore.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,24 +8,22 @@ import lombok.Setter;
 
 /**
  * Request DTO class
+ *
+ * <p>Represents incoming data for creating or updating a book.
+ * Validation is intentionally omitted as it is out of scope of this task.</p>
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookRequestDto {
-    @NotBlank(message = "Title must not be blank")
+
     private String title;
 
-    @NotBlank(message = "Author must not be blank")
     private String author;
 
-    @NotBlank(message = "ISBN must not be blank")
-    @Size(max = 32, message = "ISBN must be at most 32 characters")
     private String isbn;
 
-    @DecimalMin(value = "0.00", inclusive = true, message = "Price must be non-negative")
-    @Digits(integer = 10, fraction = 2, message = "Price must have up to 2 decimal places")
     private BigDecimal price;
 
     private String description;
