3e23d2cae550814ad636b4483c2881b5f0dad9e7
alex <ursusandwolf@protonmail.com>
2026-04-13 12:51:25 +0300

    moved EntityNotFoundException

--- src/main/java/com/lisu/onlinestore/exception/EntityNotFoundException.java
+++ src/main/java/com/lisu/onlinestore/exception/EntityNotFoundException.java
@@ -1,4 +1,4 @@
-package com.lisu.onlinestore.dao;
+package com.lisu.onlinestore.exception;
 
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message, Exception e) {
