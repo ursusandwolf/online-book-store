4683ca9347c3afba37e3b9d2c2e91e88f0207f3a
ursusandwolf <129175187+ursusandwolf@users.noreply.github.com>
2026-04-23 16:53:11 +0300

    Featute/jpa (#3)
    * Book entity added
    
    * Checkstyle config added
    
    * cy.yml config added
    
    * pom.xml spring-boot-starter-test added instead of junit
    
    * AnnotationConfigApplicationContext and service
    
    * Book entity
    
    * BookRepositoryImpl save
    
    * constructor Autowired
    
    * List<Book> findAll()
    
    * <groupId>com.h2database
    - test
    
    * AppConfig - getDataSource, getSessionFactory
    
    * properties added
    
    * Application implements CommandLineRunner
    
    * APPLICATION FAILED TO START
    
    * remove HibernateUtil.java
    
    * add lombok to project
    
    * not work with lombok (
    
    * allowPublicKeyRetrieval=true
    
    * add catch
    
    * add catch
    
    * - equals
    
    * - 17
    
    * BookDto class
    
    * up to 21 version jdk
    
    * add DTO
    
    * mapstruct added
    
    * book service impl
    
    * book service impl
    
    * verify
    
    * EntityNotFoundException
    
    * checkstyle
    
    * controller status
    
    * moved DataProcessingException
    
    * moved EntityNotFoundException
    
    * Validation is omitted
    
    * CreateBookRequestDto
    
    * findAll stream
    
    * - delete TestController.java
    
    * - removed run
    
    * - removed docs
    
    * merge approved PR
    
    * - removed docs
    
    * JpaRepository<Book, Long>
    
    * liquibase feature
    
    * soft delete
    
    * added missing endpoints and methods for BookService
    
    * - rm BookRepositoryImpl.java

--- src/main/java/com/lisu/onlinestore/controller/BookController.java
+++ src/main/java/com/lisu/onlinestore/controller/BookController.java
@@ -6,9 +6,11 @@ import com.lisu.onlinestore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,5 +38,16 @@ public class BookController {
     @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return service.create(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id, @RequestBody CreateBookRequestDto bookDto) {
+        return service.update(id, bookDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        service.deleteById(id);
     }
 }
