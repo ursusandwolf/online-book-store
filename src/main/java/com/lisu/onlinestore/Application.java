5e874de1522a064386503d7e16a28e4746a6a2ef
alex <ursusandwolf@protonmail.com>
2026-05-22 02:46:19 +0300

    feat: add TODO comments for Pagination, Sorting and Swagger

--- src/main/java/com/lisu/onlinestore/Application.java
+++ src/main/java/com/lisu/onlinestore/Application.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Application {
-
+//todo Pagination, Sorting, and Swagger
+//todo Add Pagination, Sorting, and Swagger to the existing controllers.
     private final BookService bookService;
 
     public static void main(String[] args) {
