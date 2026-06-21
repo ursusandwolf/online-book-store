b24b75420e478a31d6e62a213762cf198ae44a15
alex <ursusandwolf@protonmail.com>
2026-06-21 18:37:33 +0300

    refactor: use AuthenticationManager in AuthenticationService

--- src/main/java/com/lisu/onlinestore/config/SecurityConfig.java
+++ src/main/java/com/lisu/onlinestore/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.lisu.onlinestore.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -46,5 +48,11 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
--- src/main/java/com/lisu/onlinestore/security/AuthenticationService.java
+++ src/main/java/com/lisu/onlinestore/security/AuthenticationService.java
@@ -1,34 +1,25 @@
 package com.lisu.onlinestore.security;
 
-import com.lisu.onlinestore.dao.UserRepository;
 import com.lisu.onlinestore.dto.user.UserLoginRequestDto;
 import com.lisu.onlinestore.dto.user.UserLoginResponseDto;
-import com.lisu.onlinestore.model.User;
 import jakarta.validation.Valid;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
     private final JwtUtil tokenUtil;
-    private final PasswordEncoder passwordEncoder;
 
     public UserLoginResponseDto authenticate(@Valid UserLoginRequestDto request) {
-        Optional<User> user = userRepository.findByEmail(request.getEmail());
-        if (user.isEmpty()) {
-            throw new RuntimeException("Can't login");
-        }
-        String rawPassword = request.getPassword();
-        String userPasswordFromDb = user.get().getPassword();
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
 
-        if (!passwordEncoder.matches(rawPassword,
-                userPasswordFromDb)) {
-            throw new RuntimeException("Can't login");
-        }
-        return tokenUtil.generateToken(request.getEmail());
+        return tokenUtil.generateToken(authentication.getName());
     }
 }
